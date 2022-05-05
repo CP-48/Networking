c499644dcf8d90eba4915901e0ba21c1870473ae
asus <chaitanyapardeshi1999@hotmail.com>
2022-05-05 17:06:42 +0530

    Networking

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
+
+    [SyncVar(hook = nameof(OnHolaCountChanged))]
+    int holaCount = 0;
+
    void HandleMovement()
    {
        if(isLocalPlayer)
@@ -16,8 +20,49 @@ public class Player : NetworkBehaviour
        }
    }
 
-   void Update()
-   {
+    void Update()
+    {
        HandleMovement();
-   }
+
+       if(isLocalPlayer && Input.GetKeyDown(KeyCode.X))
+       {
+           Debug.Log("Sending hola to server!");
+           Hola();
+       }
+
+       /*if(isServer && transform.position.y > 50)
+       {
+           TooHigh();
+       }*/
+    }
+
+    public override void OnStartServer()
+    {
+        Debug.Log("Player has been spawned on the server!");
+    }
+
+    [Command]
+    void Hola()
+    {
+        Debug.Log("Received hola from client!");
+        holaCount += 1;
+        ReplyHola();
+    }
+
+    [TargetRpc]
+    void ReplyHola()
+    {
+        Debug.Log("Received hola from server!");
+    }    
+
+    void TooHigh()
+    {
+        Debug.Log("Too high!");
+    }
+
+    void OnHolaCountChanged(int oldCount, int newCount)
+    {
+        Debug.Log($"We had {oldCount} holas, but now we have {newCount} holas");
+    }
+
 }
